c8ca80ac83c82f909a89b46dedcab7c807d7c3be
Samy Pessé <samypesse@gmail.com>
2015-01-23 21:26:34 +0100

    Should translate without variables

--- test/translations.js
+++ test/translations.js
@@ -5,7 +5,11 @@ describe('Translations', function () {
     	assert.equal(tpl.render("helloworld.html"), "Hello World");
     });
 
+    it('should correctly show default value when invalid locale', function() {
+    	assert.equal(tpl.render("helloworld.html", {__locale__: "zh"}), "Hello World");
+    });
+
     it('should correctly show translate a simple message', function() {
-    	assert.equal(tpl.render("helloworld.html", {__lang__: "fr"}), "Bonjour le monde");
+    	assert.equal(tpl.render("helloworld.html", {__locale__: "fr"}), "Bonjour le monde");
     });
 });
